4107504ac86a303895203c03c3eb0e6bbe863ae4
Minjun Kim <public.velopert@gmail.com>
2022-10-05 23:37:21 +0900

    use 8080 instead of 4000

--- packages/veltrends-web/app/lib/client.ts
+++ packages/veltrends-web/app/lib/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const client = axios.create()
-client.defaults.baseURL = 'http://localhost:4000'
+client.defaults.baseURL = 'http://localhost:8080'
 client.defaults.withCredentials = true
 
 export function setClientCookie(cookie: string) {
