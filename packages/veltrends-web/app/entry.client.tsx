cbe21fcec6ac97e87385b8866a01f67be1fee86d
velopert <public.velopert@gmail.com>
2022-10-25 01:07:37 +0900

    fix: forgot some stuff on client

--- packages/veltrends-web/app/entry.client.tsx
+++ packages/veltrends-web/app/entry.client.tsx
@@ -1,14 +1,37 @@
 import { RemixBrowser } from '@remix-run/react'
-import { startTransition, StrictMode } from 'react'
+import React, { startTransition, StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 import { initializeAnalytics } from './lib/analytics'
+import createEmotionCache from './styles/createEmotionCache'
+import ClientStyleContext from './styles/client.context'
+import { CacheProvider } from '@emotion/react'
+
+interface ClientCacheProviderProps {
+  children: React.ReactNode
+}
+
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = React.useState(createEmotionCache())
+
+  const reset = React.useCallback(() => {
+    setCache(createEmotionCache())
+  }, [])
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  )
+}
 
 function hydrate() {
   startTransition(() => {
     hydrateRoot(
       document,
       <StrictMode>
-        <RemixBrowser />
+        <ClientCacheProvider>
+          <RemixBrowser />
+        </ClientCacheProvider>
       </StrictMode>,
     )
   })
